4ffd0eb78f0b9d4237156094abd84e528aa3011d
Kris Foster <krisfoster@live.ie>
2020-02-17 17:56:50 +0000

    youtube functionality

--- app.py
+++ app.py
@@ -3,16 +3,23 @@ from flask import Flask, request, jsonify
 from slack import WebClient
 from slack import RTMClient
 import os
-from threading import Thread
 import time
+import requests
+
+from threading import Thread
 from slackeventsapi import SlackEventAdapter
 
+youtube_api_token = os.environ.get('YOUTUBE_API_TOKEN')
 slack_api_token = os.environ.get('SLACK_API_TOKEN')
 slack_signing_secret = os.environ.get('SLACK_SIGNING_SECRET')
 surveillance_images_base_path = os.environ.get('SURVEILLANCE_IMAGES_BASE_PATH')
 approved_user_id = os.environ.get('APPROVED_USER_ID')
 surveillance_channel_name = os.environ.get('SURVEILLANCE_CHANNEL_NAME')
 surveillance_channel_id = ''
+
+response = requests.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCWkzkhQ3syxBjjAYwqCbzYg&key=' + youtube_api_token)
+print(response.json()['items'][0]['statistics']['subscriberCount'])
+print(response.json()['items'][0]['statistics']['viewCount'])
 
 camera = PiCamera()
 app = Flask(__name__)
@@ -44,6 +51,16 @@ def send_surveillance_image(channel_id):
     
     return 'surveillance photo uploaded :camera_with_flash'
 
+def send_youtube_stats(channel_id):
+    response = requests.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCWkzkhQ3syxBjjAYwqCbzYg&key=' + youtube_api_token)
+    subscriber_count = response.json()['items'][0]['statistics']['subscriberCount']
+    view_count response.json()['items'][0]['statistics']['viewCount']
+    response_text = ':youtube: subscribers: ' + subscriber_count + '\n' + ':youtube: views: ' + view_count
+    slack_client.chat_postMessage(
+      channel=channel_id,
+      text=response_text
+    )
+
 @slack_events_adapter.on("app_mention")
 def app_mentioned(event_data):
     print (event_data['event'])
@@ -52,13 +69,15 @@ def app_mentioned(event_data):
     channel_id = event_data['event']['channel']
     user_id = event_data['event']['user']
     if user_id == approved_user_id:
-        if 'image' in text:
+        if 'picture' in text:
             slack_client.chat_postMessage(
                 channel=channel_id,
                 text='Sure, taking a picture for you now :camera_with_flash:'
             )
             thread = Thread(target=send_surveillance_image, args=[channel_id])
             thread.start()
+        else if 'youtube' in text:
+            thread = Thread(target=send_youtube_stats, args=[channel_id])
         else:
             slack_client.chat_postMessage(
               channel=channel_id,
@@ -69,12 +88,6 @@ def app_mentioned(event_data):
               channel=channel_id,
               text='You\'re not allowed to do that :police_car:'
             )
-
-def backgroundworker(somedata,response_url):
-    payload = {"text":"your task is complete",
-                "username": "bot"}
-    requests.post(response_url,data=json.dumps(payload))
-
 
 if __name__ == '__main__':
     app.run()
