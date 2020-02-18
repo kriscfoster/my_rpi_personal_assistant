7a7babb16674f502ff684156aab19fa4fa08acaf
Kris Foster <krisfoster@live.ie>
2020-02-18 08:30:14 +0000

    refactor

---
+++ homehelper/app.py
@@ -0,0 +1,73 @@
+from picamera import PiCamera
+from flask import Flask, request, jsonify
+from threading import Thread
+from slack import RTMClient
+from slackeventsapi import SlackEventAdapter
+import slackweb
+import bedroomtv
+
+import os
+import time
+import requests
+
+youtube_api_token=os.environ.get('YOUTUBE_API_TOKEN')
+surveillance_images_base_path=os.environ.get('SURVEILLANCE_IMAGES_BASE_PATH')
+slack_signing_secret=os.environ.get('SLACK_SIGNING_SECRET')
+approved_user_id=os.environ.get('APPROVED_USER_ID')
+
+camera=PiCamera()
+app=Flask(__name__)
+
+
+slack_events_adapter=SlackEventAdapter(slack_signing_secret, "/slack/events", app)
+
+def send_surveillance_image(channel_id):
+  message='Sure, taking a picture for you now :camera_with_flash:'
+  slackweb.post_message(channel_id, message)
+
+  millis=str(round(time.time() * 1000))
+  surveillance_image_path=surveillance_images_base_path + millis + '.jpg'
+  camera.capture(surveillance_image_path)
+
+  message='Surveillance picture taken, uploading it :computer:'
+  slackweb.post_message(channel_id, message)
+  slackweb.upload_file(channel_id, surveillance_image_path, surveillance_image_path)
+
+
+def send_youtube_stats(channel_id):
+  response=requests.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCWkzkhQ3syxBjjAYwqCbzYg&key=' + youtube_api_token)
+  subscriber_count=response.json()['items'][0]['statistics']['subscriberCount']
+  view_count=response.json()['items'][0]['statistics']['viewCount']
+  response_text=':smiley: subscribers: *' + subscriber_count + '*\n' + ':eyes: views: *' + view_count + '*'
+  slackweb.post_message(channel_id, response_text)
+
+
+@slack_events_adapter.on('app_mention')
+def app_mentioned(event_data):
+  text=event_data['event']['text']
+  channel_id=event_data['event']['channel']
+  user_id=event_data['event']['user']
+  if user_id == approved_user_id:
+    if 'picture' in text:
+      thread=Thread(target=send_surveillance_image, args=[channel_id])
+      thread.start()
+    elif 'YouTube' in text:
+      thread=Thread(target=send_youtube_stats, args=[channel_id])
+      thread.start()
+    elif 'tv state' in text:
+      thread=Thread(target=bedroomtv.send_state, args=[channel_id])
+      thread.start()
+    elif 'tv off' in text:
+      thread=Thread(target=bedroomtv.turn_off, args=[channel_id])
+      thread.start()
+    elif 'tv on' in text:
+      thread=Thread(target=bedroomtv.turn_on, args=[channel_id])
+      thread.start()
+    else:
+      message='Sorry, didn\'t understand that :confused:'
+      slackweb.post_message(channel_id, message)
+  else:
+    message='You\'re not allowed to do that :police_car:'
+    slackweb.post_message(channel_id, message)
+if __name__ == '__main__':
+  app.run()
---
+++ homehelper/bedroomtv.py
@@ -0,0 +1,55 @@
+import samsungtvws
+import os
+import slackweb
+
+tv_ip_address=os.environ.get('TV_IP_ADDRESS')
+
+def get_state():
+  response=requests.get('http://' + tv_ip_address + ':8001/api/v2/')
+  state=response.json()['device']['PowerState']
+  return state
+
+def toggle_state():
+  token_file=os.path.dirname(os.path.realpath(__file__)) + '/tv-token.txt'
+  tv=samsungtvws.SamsungTVWS(host=tv_ip_address, port=8002, token_file=token_file)
+  tv.shortcuts().power()
+
+def send_state(channel_id):
+  tv_state=get_state()
+  message=':tv: tv state is: *' + tv_state + '*'
+  slackweb.post_message(channel_id, message)
+
+
+def turn_off(channel_id):
+  tv_state=get_state()
+
+  message=''
+
+  if tv_state == 'on':
+    message=':tv: tv state is *on*, turning *off*'
+  else:
+    message=':tv: tv state is already *off*'
+
+  slackweb.post_message(channel_id, message)
+
+  if tv_state == 'on':
+    toggle_state()
+    message=':tv: tv turned *off*'
+    slackweb.post_message(channel_id, message)
+
+def turn_on(channel_id):
+  tv_state=get_state()
+
+  message=''
+
+  if tv_state != 'on':
+    message=':tv: tv state is *off*, turning *on*'
+  else:
+    message=':tv: tv state is already *on*'
+
+  slackweb.post_message(channel_id, message)
+
+  if tv_state != 'on':
+    toggle_state()
+    message=':tv: tv turned *on*'
+    slackweb.post_message(channel_id, message)
